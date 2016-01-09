fe9627f1099c3220adf245dd718988cbc588fba0
ezbc <ezbc@astro.wisc.edu>
2016-01-08 18:15:59 -0600

    read in train data as dataframe

---
+++ scripts/myfuncs.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+def write_submission.py
--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 
-def read_data():
+import pandas as pd
 
-    pass
+def read_data(filename):
+
+    df = pd.read_csv(filename,
+                     sep=',',
+                     header=0,
+                     )
+
+    data = df.date_account_created[:10]
+    print data
+    #print pd.to_datetime(data, format='%f')
 
 def main():
 
-    read_data()
+    DIR_DATA = '../data/'
+    FILENAME_TRAIN = DIR_DATA + 'train_users_2.csv'
+
+    df_train = read_data(FILENAME_TRAIN)
 
 if __name__ == '__main__':
 
